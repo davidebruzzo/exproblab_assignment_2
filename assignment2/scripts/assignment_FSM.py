ed048850d77fb113b5bbaf798e05614e0b8913dc
davidebruzzo <62437632+davidebruzzo@users.noreply.github.com>
2023-02-03 11:56:28 +0000

    Add files via upload
    Integrated assignment 1 into second assignment repo

---
+++ assignment2/scripts/assignment_FSM.py
@@ -0,0 +1,347 @@
+#!/usr/bin/env python
+
+## @package exp1_assignment
+# \file assignment_FSM.py
+# \brief This file represent the FSM implementation
+# \author Davide Bruzzo
+# \version 1.0
+# \date 18/11/2022
+#
+# \details
+#
+#  Subscribes to: <BR>
+#   [None]
+#
+#  Publishes to: <BR>
+#   [None]
+#
+#  Service: <BR>
+#   [None]
+#
+# This file implements all the nodes of the final state machine that I thought
+#
+import roslib
+import rospy
+import smach
+import smach_ros
+import time
+import random
+
+# Import constant name defined to structure the architecture.
+from arch_skeleton import architecture_name_mapper as anm
+# Import the armor client class
+from armor_client import ArmorClient
+
+from my_helper import MyHelper
+
+# Import used messages defined within the ROS architecture.
+from arch_skeleton.msg import Gesture, Point, Speech, PlanAction, PlanGoal, ControlAction, ControlGoal
+
+
+client = ArmorClient("my_client","ontology2")
+
+
+path = '/root/ros_ws/src/topological_map/'
+
+
+# define state Wait Ontology
+class WaitForOntology(smach.State):
+    ##
+    # \class WaitForOntology
+    # \brief This class defines the WaitForOntology state
+    #
+    # This class implements the WaitForOntology state of the final state machine, it is done once to create the ontology as the requirements of the assignments asked.
+    # It calls the LoadOntology function to do that. The LoadOntology has a particular structure with loops that do the same instruction two or three times.
+    # This is not an error but vouluntary done due to some bugs of the armor server. This is a solution I found to make things working and overcoming the bugs.
+    #
+
+    def __init__(self, my_helper):
+        ##
+        # \brief This function is the constructor of the WaitForOntology class
+        # \param my_helper
+        # \return None
+        #
+        self._helper = my_helper
+        smach.State.__init__(self,
+                             outcomes=['received','battery_low', 'charged', 'planned_to_urgent', 'visited'])
+
+
+    def execute(self, userdata):
+        ##
+        # \brief This function is the excute function of the WaitForOntology state
+        # \param userdata
+        # \return None
+        #
+        rospy.loginfo('Executing state WaitOntology')
+        self._helper.NewLoadOntology(client, path)
+
+
+        time.sleep(0.5)
+        return 'received' #we need to enter in recharging state to see if the robot has  100% battery
+
+class Recharging(smach.State):
+    ##
+    # \class Recharging
+    # \brief This class defines the Recharging state
+    #
+    # This class implements the Recharging state of the final state machine, it moves the
+    # robot to the E location which is the one for recharging.
+    # Then checks in every iteration the state of the battery to see if robot has completed recharging.
+    #
+    def __init__(self, my_helper):
+        ##
+        # \brief This function is the constructor of the Recharging class
+        # \param my_helper
+        # \return None
+        #
+        self._helper = my_helper
+
+        smach.State.__init__(self,
+                             outcomes=['received','battery_low', 'charged', 'planned_to_urgent', 'visited'],
+                             input_keys=['recharging_counter_in'])
+
+
+    def execute(self, userdata):
+        ##
+        # \brief This function is the excute function of the Recharging state
+        # \param userdata
+        # \return None
+        #
+
+        self._helper.MoveRobot(client, "E")
+        print("Robot moved to E ")
+        rospy.sleep(0.2)
+        print("ROBOT IS IN RECHARGING")
+        self._helper.mutex.acquire()
+        try:
+            # If the battery is no low anymore take the `charged` transition.
+            if not self._helper.is_battery_low():
+                self._helper.reset_battery()  # Reset the state variable related to the stimulus.
+                print("BATTERY CHARGED")
+                return 'charged'
+        finally:
+            # Release the mutex to unblock the `self._helper` subscription threads if they are waiting.
+            self._helper.mutex.release()
+        time.sleep(0.5)
+        return random.choice(['received', 'battery_low', 'visited', 'planned_to_urgent'])
+
+
+
+
+class Plan_To_Urgent(smach.State):
+    ##
+    # \class Plan_To_Urgent
+    # \brief This class defines the Plan_To_Urgent state
+    #
+    # This class implements the Plan_To_Urgent state of the final state machine, it gets the
+    # room reachable and the urgent ones. Then decides which room robot should reach. It calls the method for creting the path through 'via_points',
+    # simulating in this way the behaviour of the real robot, then calls the function to move the robot in the room for the ontology.
+    # This is made always checking in every loop the state of the battery to see if robot has to go recharging.
+    #
+    def __init__(self, my_helper):
+        ##
+        # \brief This function is the constructor of the Plan_To_Urgent class
+        # \param my_helper
+        # \return None
+        #
+        self._helper = my_helper
+        smach.State.__init__(self,
+                             outcomes=['received','battery_low', 'charged', 'planned_to_urgent', 'visited'],
+                             input_keys=['Plan_To_Urgent_counter_in'],
+                             output_keys=['Plan_To_Urgent_var', 'resultRoom'])
+
+
+    def execute(self, userdata):
+        ##
+        # \brief This function is the excute function of the Plan_To_Urgent state
+        # \param userdata
+        # \return None
+        #
+
+        self._helper.UpdateNowRobot(client)
+        self._helper.Reasoning(client)
+        uRooms = self._helper.GetUrgentRooms(client)
+        rRooms = self._helper.GetReachableRooms(client)
+
+        resultR = self._helper.DecideUrgentReachable(rRooms, uRooms)
+        userdata.resultRoom = resultR
+        try:
+    # If the battery is low.
+            if self._helper.is_battery_low():
+                print("Battery low arrived ... going to RECHARGING state")
+                if(self._helper.IsRoom(resultR)):
+                    print("Robot found in a room, planning for coming back to E location...")
+                    rospy.sleep(0.5)
+                    checkR = self._helper.GetReachableRooms(client)
+                    corridor = self._helper.WhichCorridor(checkR)
+                    self._helper.MoveRobot(client, corridor)
+                    print("Robot moved to CORRIDOR: ",  corridor)
+                return 'battery_low'
+            else:
+                print("URGENT ROOMS: ", uRooms)
+                print("REACHABLE ROOMS: " ,rRooms)
+                print("RANDOMLY CHOSEN ONE: ", resultR)
+                time.sleep(1)
+                if(self._helper.IsRoom(resultR)):
+                    listPath = self._helper.PlanningPath()
+                    if(listPath[0]):
+                        print("\nPath planned!\n")
+                        userdata.Plan_To_Urgent_var = listPath[1]
+                        self._helper.MoveRobot(client, resultR)
+                        print("I'm moving to the urgent room: ", resultR)
+                        rospy.sleep(1)
+                        self._helper.Reasoning(client)
+                        return 'planned_to_urgent'
+                    else:
+                        return 'battery_low'
+                else:
+                    #waiting for planning to in corridor
+                    listPathC = self._helper.PlanningPath()
+                    if(listPathC[0]):
+                        self._helper.MoveRobot(client, resultR)
+                        # I decided to not call server for going to corridor cause i imagined passing from one corridor to another immediately
+                        print("Robot is moving to corridor: ", resultR)
+                        rospy.sleep(0.5)
+                        self._helper.Reasoning(client)
+                        return random.choice(['received', 'charged', 'visited'])
+
+                    else:
+                        return 'battery_low'
+        finally:
+            print()
+
+
+class Visit_Room(smach.State):
+    ##
+    # \class Visit_Room
+    # \brief This class defines the Visit_Room state
+    #
+    # This class implements the Visit_Room state of the final state machine, it receives the
+    # room chosen and the set of 'via_points' by 'Plan_To_Urgent' state. It calls the method for moving along the path created,
+    # simulating in this way the behaviour of the real robot, then calls the function to simulate the robot visiting the room.
+    # This is made always checking in every loop the state of the battery to see if robot has to go recharging.
+    #
+
+    def __init__(self, my_helper):
+        ##
+        # \brief This function is the constructor of the Visit_Room class
+        # \param my_helper
+        # \return None
+        #
+        self._helper = my_helper
+        # initialisation function, it should not wait
+        smach.State.__init__(self,
+                             outcomes=['received','battery_low', 'charged', 'planned_to_urgent', 'visited'],
+                             input_keys=['Plan_To_Urgent_var', 'resultRoom'],
+                             output_keys=['Visit_Room_counter_out'])
+
+    def execute(self, userdata):
+        ##
+        # \brief This function is the excute function of the Visit_Room state
+        # \param userdata
+        # \return None
+        #
+        roomChosen = userdata.resultRoom
+        plan = userdata.Plan_To_Urgent_var
+
+        if(self._helper.MovingAlongPath(plan)):
+            print("Reached room: ", roomChosen)
+        else:
+            print("Robot found with battery low during its path toward the room \nGoing to recharching state...")
+            return 'battery_low'
+
+
+        if(self._helper.VisitingRoom(client)):
+            print("\nFULLY VISITED ROOM: ", roomChosen, "\n")
+            self._helper.UpdateNowRobot(client)
+            self._helper.UpdateVisitedRoom(client, roomChosen)
+            self._helper.Reasoning(client)
+            rospy.sleep(0.5)
+            return 'visited'
+        else:
+            #battery_low arrived, moving robot to E location
+            print("BATTERY LOW")
+            if(self._helper.IsRoom(roomChosen)):
+                self._helper.UpdateNowRobot(client)
+                self._helper.Reasoning(client)
+                print("Robot found in a room, planning for coming back to E location...")
+                rospy.sleep(0.5)
+                checkR = self._helper.GetReachableRooms(client)
+                corridor = self._helper.WhichCorridor(checkR)
+                print("CORRIDOR CHOSEN: ", corridor)
+                self._helper.MoveRobot(client, corridor)
+                self._helper.UpdateNowRobot(client)
+                self._helper.Reasoning(client)
+            return 'battery_low'
+
+def main():
+    ##
+    # \brief This function is the main of assignment_FSM
+    # \param None
+    # \return None
+    #
+    rospy.init_node('exercise_robot_state_machine')
+
+    my_helper = MyHelper()
+
+    # Get the initial robot pose from ROS parameters.
+    robot_pose_param = rospy.get_param(anm.PARAM_INITIAL_POSE, [0, 0])
+    # Initialise robot position in the `robot_state`, as required by the plan anc control action servers.
+    my_helper.init_robot_pose(Point(x=robot_pose_param[0], y=robot_pose_param[1]))
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['container_interface'])
+    sm.userdata.sm_counter = 0
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('WAIT ONTOLOGY', WaitForOntology(my_helper),
+                               transitions={'received':'RECHARGING',
+                                            'battery_low':'WAIT ONTOLOGY',
+                                            'charged':'WAIT ONTOLOGY',
+                                            'planned_to_urgent':'WAIT ONTOLOGY',
+                                            'visited':'WAIT ONTOLOGY'},
+                               remapping={'WaitForOntology_counter_in':'sm_counter',
+                                          'WaitForOntology_counter_out':'sm_counter'})
+        smach.StateMachine.add('RECHARGING', Recharging(my_helper),
+                               transitions={'received':'RECHARGING',
+                                            'battery_low':'RECHARGING',
+                                            'charged':'PLAN TO URGENT',
+                                            'planned_to_urgent':'RECHARGING',
+                                            'visited':'RECHARGING'},
+                               remapping={'recharging_counter_in':'sm_counter',
+                                          'recharging_counter_out':'sm_counter'})
+        smach.StateMachine.add('PLAN TO URGENT', Plan_To_Urgent(my_helper),
+                               transitions={'received':'PLAN TO URGENT',
+                                            'battery_low':'RECHARGING',
+                                            'charged':'PLAN TO URGENT',
+                                            'planned_to_urgent':'VISIT ROOM',
+                                            'visited':'PLAN TO URGENT'},
+                               remapping={'Plan_To_Urgent_counter_in':'sm_counter',
+                                          'Plan_To_Urgent_counter_out':'sm_counter'})
+
+        smach.StateMachine.add('VISIT ROOM', Visit_Room(my_helper),
+                               transitions={'received':'VISIT ROOM',
+                                            'battery_low':'RECHARGING',
+                                            'charged':'VISIT ROOM',
+                                            'planned_to_urgent':'VISIT ROOM',
+                                            'visited':'PLAN TO URGENT'},
+                               remapping={'Visit_Room_counter_in':'sm_counter',
+                                          'Visit_Room_counter_out':'sm_counter'})
+
+
+    # Create and start the introspection server for visualization
+    sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+    sis.start()
+
+    # Execute the state machine
+    outcome = sm.execute()
+
+    # Wait for ctrl-c to stop the application
+    rospy.spin()
+    sis.stop()
+
+
+if __name__ == '__main__':
+    main()
--- assignment2/src/move_prova.cpp
+++ assignment2/src/move_prova.cpp
@@ -34,13 +34,13 @@ private:
   aruco::MarkerDetector mDetector_;
   std::vector<aruco::Marker> markers_;
   aruco::CameraParameters camParam_;
-  
+
   std::map<int, bool> detected_marker;   //!< Map to store if a marker has been detected or not
   std::vector<int> id_marker;           //!< Vector to store the ids of the detected markers
   int markers_number;
-  
+
   assignment2::RoomInformation srv;
-  
+
 
   // node params
   double marker_size_;
@@ -50,21 +50,21 @@ private:
   ros::NodeHandle nh_;
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
-  
+
   //????????????????????????????????????????????
 
  //
 
   cv::Mat inImage_;
-  
+
 public:
   MyArucoMarkerPublisher() :
       nh_("~"), it_(nh_), useCamInfo_(true)
   {
     image_sub_ = it_.subscribe("/robot/camera1/image_raw", 1, &MyArucoMarkerPublisher::image_callback, this);
     //image_pub_ = it_.advertise("result", 1);
-  
-    
+
+
     nh_.param<bool>("use_camera_info", useCamInfo_, false);
     camParam_ = aruco::CameraParameters();
   }
@@ -72,63 +72,63 @@ public:
   void image_callback(const sensor_msgs::ImageConstPtr& msg)
   {
     //bool publishImage = image_pub_.getNumSubscribers() > 0;
-    
+
     ros::Time curr_stamp = msg->header.stamp;
     cv_bridge::CvImagePtr cv_ptr;
-    
+
     try
     {
       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
       inImage_ = cv_ptr->image;
-   
+
       // clear out previous detection results
       markers_.clear();
 
       // ok, let's detect
       mDetector_.detect(inImage_, markers_, camParam_, marker_size_, false);
 
-		
+
         for (std::size_t i = 0; i < markers_.size(); ++i)
         {
-          
-          
+
+
           int marker_id = markers_[i].id;
-          
+
           if(!detected_marker[marker_id])
             {
                 detected_marker[marker_id] = true;
-                
+
                 std::cout << "The id of the detected marker detected is: ";
           	std::cout << markers_.at(i).id << " ";
-          	std::cout << std::endl;                                                   
-                
+          	std::cout << std::endl;
+
                 srv.request.id = markers_.at(i).id;
-                
+
           	if(roomInfoClient.call(srv)){
-          	
+
           		      // print the response
                     printf("Room server response:\n Room: %s;\n Coord: (%f,%f);\n", srv.response.room.c_str(), srv.response.x, srv.response.y);
                     for (std::size_t j = 0; j < srv.response.connections.size(); ++j){
                         printf("Connected to room: %s Through door: %s;\n", srv.response.connections.at(j).connected_to.c_str(), srv.response.connections.at(j).through_door.c_str());
-                    }    	
-          	
+                    }
+
           		assignment2::InfoRoom room_info_msg;
-          		
+
           		room_info_msg.room = srv.response.room;
           		room_info_msg.x = srv.response.x;
           		room_info_msg.y = srv.response.y;
-          		
+
           		for(std::size_t j = 0; j < srv.response.connections.size(); j++){
-          		
+
           			room_info_msg.connections.push_back(srv.response.connections.at(j));
-          		
+
           		}
-          		pub_info_room.publish(room_info_msg);    	
+          		pub_info_room.publish(room_info_msg);
           	}
           	//srv.request.id = markers_.at(i).id;
         }
        }
-        
+
 
 
     }
@@ -148,47 +148,47 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "move_prova");
 
   ROS_INFO("Starting moving arm ...");
- 
+
   // Precondition: Valid clock
   ros::NodeHandle nh;
-  
+
   pub_joint_1 = nh.advertise<std_msgs::Float64>("/robot/joint1_position_controller/command",100);
   pub_joint_2 = nh.advertise<std_msgs::Float64>("/robot/joint2_position_controller/command",100);
   pub_joint_3 = nh.advertise<std_msgs::Float64>("/robot/joint3_position_controller/command",100);
-  
+
   pub_info_room = nh.advertise<assignment2::InfoRoom>("/info_room_sm",100);
-  
-  /*while(pub_info_room.getNumSubscribers() < 1)
+
+  while(pub_info_room.getNumSubscribers() < 1)
   {
   	ros::Duration(0.1).sleep();
-  }*/
-  
+  }
+
   roomInfoClient = nh.serviceClient<assignment2::RoomInformation>("/room_info");
   roomInfoClient.waitForExistence();
-  
+
   MyArucoMarkerPublisher node;
-  
+
   int flagCycle = 0;
-  
-  
-   while(ros::ok()){  
-   
-   
+
+
+   while(ros::ok()){
+
+
    while(flagCycle!=-1){
-	
+
 	   	if(flagCycle < 30){
 	   	position.data = 6.28;
 	  	pub_joint_1.publish(position);
 	   	//printf("Rotation\n");
-	   	
+
 	   	}
-	   	
+
 	   	if(flagCycle > 30 && flagCycle < 50){
 	   	position.data = 0.0;
 	  	pub_joint_1.publish(position);
 	  	position.data = 0.4;
 	  	pub_joint_3.publish(position);
-	   	
+
 	   	}
 	   	if(flagCycle > 50 && flagCycle < 60){
 	   	position.data = 1.57;
@@ -196,24 +196,24 @@ int main(int argc, char** argv)
 	  	position.data = 1.57;
 	  	pub_joint_2.publish(position);
 	  	position.data = 1.4;
-	  	pub_joint_3.publish(position);	   	
+	  	pub_joint_3.publish(position);
 	   	}
 	   	if(flagCycle == 60){
 	   		flagCycle = -1;
 	   		printf("FULL ROTATION DONE\n");
-	   		break;	
+	   		break;
 	   	}
-	   
+
 	   flagCycle++;
 	   ros::Duration(1.0).sleep();
 	   ros::spinOnce();
-	   
-	   	
+
+
 	}
-   	
+
    ros::spinOnce();
-   
+
    }
-  
+
   return 0;
  }
